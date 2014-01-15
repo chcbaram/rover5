6e3064e06b33aee5e985cf402644dadf1bb80de6
Baram <chcbaram@paran.com>
2014-01-15 23:21:51 +0900

    VCOM -> COM1 으로 변경

--- rover5_SmartBD/Build/main.elf.d
+++ rover5_SmartBD/Build/main.elf.d
@@ -1,9 +1,9 @@
-Hw_Pwm.o: ../LIB_HW/Hw_Pwm.c ../LIB_HW/Hw_Pwm.h ../LIB_HW/Hw.h \
- ../LIB_HW/Hw_Regs.h ../LIB_HW/Hw_Define.h ../LIB_HW/Hw_PLL.h \
+usb_int.o: ../LIB_USB/DRV/usb_int.c ../LIB_USB/DRV/usb_lib.h \
+ ../LIB_USB/hw_config.h ../LIB_HW/Hw.h ../LIB_HW/Hw_Regs.h \
+ ../LIB_HW/Hw_Define.h ../LIB_HW/Hw_PLL.h ../LIB_HW/Hw.h \
  ../LIB_HW/Hw_DMA.h ../LIB_HW/Hw_Uart.h ../LIB_HW/Hw_Led.h \
  ../LIB_HW/Hw_ISR.h ../LIB_HW/Hw_Timer.h ../LIB_HW/Hw_I2C.h \
  ../LIB_HW/Hw_I2C_SW.h ../LIB_HW/Hw_SPI.h ../LIB_HW/Hw_Adc.h \
-<<<<<<< HEAD
  ../LIB_HW/Hw_Pwm.h ../LIB_HW/Hw_Si47xx.h ../LIB_HW/Hw_N5110G.h \
  ../LIB_HW/Hw_VBus.h ../LIB_HW/Hw_KeyPad.h ../LIB_HW/Hw_7Seg.h \
  ../LIB_HW/Hw_CLcd.h ../LIB_HW/Hw_Cds.h ../LIB_HW/Hw_DcMotor.h \
@@ -13,14 +13,10 @@ Hw_Pwm.o: ../LIB_HW/Hw_Pwm.c ../LIB_HW/Hw_Pwm.h ../LIB_HW/Hw.h \
  ../LIB_USB/DRV/usb_def.h ../LIB_USB/DRV/usb_core.h \
  ../LIB_USB/DRV/usb_init.h ../LIB_USB/DRV/usb_sil.h \
  ../LIB_USB/DRV/usb_mem.h ../LIB_USB/DRV/usb_int.h
-=======
- ../LIB_HW/Hw_Si47xx.h ../LIB_HW/Hw_N5110G.h ../LIB_HW/Hw_VBus.h \
- ../LIB_HW/Hw_KeyPad.h ../LIB_HW/Hw_7Seg.h ../LIB_HW/Hw_CLcd.h \
- ../LIB_HW/Hw_Cds.h ../LIB_HW/Hw_DcMotor.h ../LIB_HW/Hw_VCom.h \
- ../LIB_LB/Lb_Printf.h ../LIB_HW/Hw.h
->>>>>>> fdfbab5dfe0750cc005093ae61960dc4965abfc7
 
-../LIB_HW/Hw_Pwm.h:
+../LIB_USB/DRV/usb_lib.h:
+
+../LIB_USB/hw_config.h:
 
 ../LIB_HW/Hw.h:
 
@@ -29,6 +25,8 @@ Hw_Pwm.o: ../LIB_HW/Hw_Pwm.c ../LIB_HW/Hw_Pwm.h ../LIB_HW/Hw.h \
 ../LIB_HW/Hw_Define.h:
 
 ../LIB_HW/Hw_PLL.h:
+
+../LIB_HW/Hw.h:
 
 ../LIB_HW/Hw_DMA.h:
 
@@ -70,4 +68,24 @@ Hw_Pwm.o: ../LIB_HW/Hw_Pwm.c ../LIB_HW/Hw_Pwm.h ../LIB_HW/Hw.h \
 
 ../LIB_LB/Lb_Printf.h:
 
-../LIB_HW/Hw.h:
+../LIB_USB/platform_config.h:
+
+../LIB_USB/DRV/usb_type.h:
+
+../LIB_USB/usb_conf.h:
+
+../LIB_USB/DRV/usb_type.h:
+
+../LIB_USB/DRV/usb_regs.h:
+
+../LIB_USB/DRV/usb_def.h:
+
+../LIB_USB/DRV/usb_core.h:
+
+../LIB_USB/DRV/usb_init.h:
+
+../LIB_USB/DRV/usb_sil.h:
+
+../LIB_USB/DRV/usb_mem.h:
+
+../LIB_USB/DRV/usb_int.h:
